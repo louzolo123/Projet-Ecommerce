9e660878f10e05e5e721b770996e6fca02583d54
louzolo123 <156509693+louzolo123@users.noreply.github.com>
2024-02-09 22:12:43 +0100

    deuxieme commit

--- apps/urls.py
+++ apps/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('category/' , views.category_list , name='category'),
     path('displayCategoryOfProducts/<int:productCategory_id>' , views.display_list_category_of_product , name='displayCategoryOfProducts'),
     path('vendor/' , views.vendor_list , name='vendor'),
-    path('vendorDetail/<int:vendor_id>' , views.vendor_detail , name='vendorDetail'),    
+    path('vendorDetail/<int:vendor_id>' , views.vendor_detail , name='vendorDetail'),
+    path('productDetail/<int:product_id>' , views.product_detail , name='productDetail'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -6,15 +6,21 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     products = Product.objects.order_by('-id').all()
+    category = Category.objects.order_by('-id').all()
+    product = Product.objects.all()[:3]
     context = {
-        'products' : products
+        'products' : products,
+        'category' : category,
+        'product' : product
     }
     return render(request , 'apps/index.html' , context)
 @login_required
 def category_list(request):
     categories = Category.objects.all()
+    product = Product.objects.all()[:3]
     context = {
-        'categories' : categories
+        'categories' : categories,
+        'product' : product
     }
     return render(request , 'apps/category_list.html', context)
 @login_required
@@ -34,14 +40,28 @@ def vendor_list(request):
     vendors_count = vendors.count()
     context = {
         'vendor':vendors,
-        'vendors_count' : vendors_count
+        'vendors_count' : vendors_count,
+        
+        
     }
     return render(request , 'apps/listVendors.html', context)
 
 @login_required
 def vendor_detail(request , vendor_id):
-    vendors = Vendor.objects.get(id=vendor_id)
+    vendor = Vendor.objects.get(id=vendor_id)
+    products = Product.objects.filter(vendor = vendor , products_status = 'published')
+    product = Product.objects.all()[:3]
+    category = Category.objects.order_by('-id').all()
     context = {
-        "vendors": vendors
+        "vendor": vendor,
+        'products' : products,
+        'product' : product,
+        'category' : category,
     }
     return render(request , 'apps/vendorDetail.html', context)
+def product_detail(request , product_id):
+    product = Product.objects.get(id=product_id)
+    context  = {
+        'product': product,
+    }
+    return render(request , 'apps/productDetail.html', context)
